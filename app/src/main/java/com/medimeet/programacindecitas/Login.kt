82478bfc6d6f1ca28710c7207dba5243e1d758b3
Steven <martin170520@gmail.com>
2024-05-05 13:25:25 -0500

    cambio de login al lugar correcto

--- app/src/main/java/com/medimeet/programacindecitas/Login.kt
+++ app/src/main/java/com/medimeet/programacindecitas/Login.kt
@@ -1,4 +1,4 @@
-package com.medimeet.programacindecitas.ui.theme
+package com.medimeet.programacindecitas
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.medimeet.programacindecitas.ui.theme.ProgramaciónDeCitasTheme
 
 class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
