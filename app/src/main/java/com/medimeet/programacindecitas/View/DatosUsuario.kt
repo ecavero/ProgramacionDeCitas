4257fbde4b2068b0c2ca0f72d51a1f16184bd03e
Steven <martin170520@gmail.com>
2024-05-07 14:02:44 -0500

    agregando import y agregando archivo Datos usuario

---
+++ app/src/main/java/com/medimeet/programacindecitas/View/DatosUsuario.kt
@@ -0,0 +1,15 @@
+package com.medimeet.programacindecitas.View
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun DatosUsuarios(navController: NavHostController){
+    Box(modifier = Modifier){
+
+    }
+
+}
+
